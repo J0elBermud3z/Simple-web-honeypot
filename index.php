1168ee8fec5a13cf8fa761f20bca8ef26ad3f8ad
Joel <joelnicolasbermudezdiaz@gmail.com>
2025-03-24 00:05:47 -0300

    feat(login_php): Now we can log the login info.

--- index.php
+++ index.php
@@ -168,19 +168,23 @@
 
 
   <main class="form-signin w-100 m-auto">
-    <form method='POST' action='index.php'>
+    <form method="GET" action="index.php">
       <h1 class="h2 mb-3 fw-normal">Bank sign in</h1>
+
       <div class="form-floating">
-        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <input type="email" class="form-control" id="floatingInput" name="username" placeholder="name@example.com"
+          required>
         <label for="floatingInput">Email address</label>
       </div>
+
       <div class="form-floating">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password"
+          required>
         <label for="floatingPassword">Password</label>
       </div>
 
       <div class="form-check text-start my-3">
-        <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+        <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault" name="remember-me">
         <label class="form-check-label" for="flexCheckDefault">
           Remember me
         </label>
@@ -191,9 +195,7 @@
         }
       ?>
       <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
-      <p class="mt-5 mb-3 text-body-secondary">&copy; 1998–2025</p>
     </form>
-
   </main>
   <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
 </body>
